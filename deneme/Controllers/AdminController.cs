cdfb787cd36f68c667ec521f0627fe47dbc857ff
Filiz Molla <filiz.molla01@gmail.com>
2022-08-23 11:54:07 +0300

    admin paneli create edit delete tamamlandi

--- deneme/Controllers/AdminController.cs
+++ deneme/Controllers/AdminController.cs
@@ -21,9 +21,86 @@ namespace deneme.Controllers
 
         public IActionResult Listele()
         {
-
             IEnumerable<TblPost> objCategoryList = _db.TblPosts.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TblPost obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TblPosts.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Listele");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var post = _db.TblPosts.Find(id);
+            if(post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TblPost post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.UpdatedTime = DateTime.Now;
+                post.CreatedTime = DateTime.Now;//TODO: created time update time ile ayni
+                _db.TblPosts.Update(post);
+                _db.SaveChanges();
+                return RedirectToAction("Listele");
+            }
+            return View(post);
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var post = _db.TblPosts.Find(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.TblPosts.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.TblPosts.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Listele");
         }
 
 
--- deneme/Models/TblPost.cs
+++ deneme/Models/TblPost.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace deneme.Models
 {
     public partial class TblPost
     {
+        [Key]
+
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Body { get; set; }
