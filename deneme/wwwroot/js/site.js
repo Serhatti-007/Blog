e191a7f7ed2294f6d1f852b420767d50f7f421af
Filiz Molla <filiz.molla01@gmail.com>
2022-08-26 09:16:45 +0300

    yeni post eklerken ve editlerken font degisimi ve liste vs ekleme.

--- deneme/wwwroot/js/site.js
+++ deneme/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+ClassicEditor
+    .create(document.querySelector('#body'), {
+        toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
